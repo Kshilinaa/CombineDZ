1e1afc1976380880c588fdd16983944221346347
Kshilinaa <ksusha_2605@mail.ru>
2024-05-20 15:39:41 +0300

    init

---
+++ CombineDZ/CombineDZ/CombineDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDZApp.swift
+//  CombineDZ
+//
+//  Created by Ксения Шилина on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
